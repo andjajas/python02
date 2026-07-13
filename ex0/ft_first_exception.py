6f03f2071bc87dafa5739ce189898c8bb053ef2b
andjajas <adjajasaputra@gmail.com>
2026-07-13 23:04:49 +0200

    ex0 start

--- ex0/ft_first_exception.py
+++ ex0/ft_first_exception.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python3
-#test
+def input_temperature(temp_str) -> int:
+	temp = int(temp_str)
+	return(temp)
+
+
+def test_temperature() -> None:
+	input_temperature("25")
+	input_temperature("abc")
